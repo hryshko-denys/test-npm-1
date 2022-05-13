278510c33e09415de292fb81fe7fe2ab357efd8d
hryshko-denys <dh@everstake.one>
2022-05-13 14:04:12 +0300

    change config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,20 @@
 import { PublicKey, Connection } from '@solana/web3.js';
+import { StakePoolProgram } from '../service/stakepool-program';
 
 const TESTNET_PROVIDER_URL = 'https://api.testnet.solana.com';
 const MAINNET_PROVIDER_URL = 'https://api.mainnet-beta.solana.com';
 
-export type ClusterType = "mainnet-beta" | "testnet";
+export type ClusterType = 'mainnet-beta' | 'testnet';
 
-const TESTNET_STAKEPOOL_ACCOUNT = "4QRsVADHap1AgtpryYB2vz4htG3ysJcaXFkcFqkGjaF1"
-const MAINNET_STAKEPOOL_ACCOUNT = "GUAMR8ciiaijraJeLDEDrFVaueLm9YzWWY9R7CBPL9rA"
+const TESTNET_STAKEPOOL_ACCOUNT = '4QRsVADHap1AgtpryYB2vz4htG3ysJcaXFkcFqkGjaF1';
+const MAINNET_STAKEPOOL_ACCOUNT = 'GUAMR8ciiaijraJeLDEDrFVaueLm9YzWWY9R7CBPL9rA';
 
-const TESTNET_STAKEPOOL_PROGRAM_ID = "CgymamZFh5aVZhoSRtV2QrEZ58X4N1o2H5ZQPF4L5jj8"
-const MAINNET_STAKEPOOL_PROGRAM_ID = "EverSFw9uN5t1V8kS3ficHUcKffSjwpGzUSGd7mgmSks"
+const TESTNET_STAKEPOOL_PROGRAM_ID = 'CgymamZFh5aVZhoSRtV2QrEZ58X4N1o2H5ZQPF4L5jj8';
+const MAINNET_STAKEPOOL_PROGRAM_ID = 'EverSFw9uN5t1V8kS3ficHUcKffSjwpGzUSGd7mgmSks';
 
 export class ESolConfig {
   eSOLProgramId: PublicKey;
-  eSOLStakePoolAddress: PublicKey
+  eSOLStakePoolAddress: PublicKey;
   seedPrefixDaoState = 'dao_state';
   seedPrefixCommunityToken = 'community_token';
   seedPrefixCommunityTokenStakingRewards = 'c_t_staking_rewards';
@@ -24,18 +25,20 @@ export class ESolConfig {
 
   constructor(clusterType: ClusterType) {
     switch (clusterType) {
-      case "testnet":
+      case 'testnet':
         this.eSOLStakePoolAddress = new PublicKey(TESTNET_STAKEPOOL_ACCOUNT);
         this.eSOLProgramId = new PublicKey(TESTNET_STAKEPOOL_PROGRAM_ID);
         this.connection = new Connection(TESTNET_PROVIDER_URL);
         break;
-      case "mainnet-beta":
+      case 'mainnet-beta':
         this.eSOLStakePoolAddress = new PublicKey(MAINNET_STAKEPOOL_ACCOUNT);
         this.eSOLProgramId = new PublicKey(MAINNET_STAKEPOOL_PROGRAM_ID);
         this.connection = new Connection(MAINNET_PROVIDER_URL);
+
+        StakePoolProgram.changeProgramId('EverSFw9uN5t1V8kS3ficHUcKffSjwpGzUSGd7mgmSks');
         break;
       default:
-        throw new Error("clusterType must be specified");
+        throw new Error('clusterType must be specified');
     }
   }
 }
